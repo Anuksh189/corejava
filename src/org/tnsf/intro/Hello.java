3d38c05647d25cf0d7b2c35e7354661cd953efa5
Avita <Avita@DESKTOP-BSABA2I>
2023-07-20 17:33:12 +0530

    First Commit

---
+++ src/org/tnsf/intro/Hello.java
@@ -0,0 +1,29 @@
+package org.tnsf.intro;
+
+import java.util.Scanner;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+  Scanner obj = new Scanner(System.in);
+  
+  /*System.out.println("Enter your Number");
+  int num= obj.nextInt();
+  System.out.println("The number is:"+num);*/
+  
+  /*System.out.println("Enter your Number");
+  float num1= obj.nextFloat();
+  System.out.println("The number is:"+num1);*/
+  
+  /*System.out.println("Enter The Char");
+  char a= obj.next().charAt(0);
+  System.out.println("The character is:"+a);*/
+  
+  System.out.println("Enter the string");
+  String str= obj.nextLine();
+  System.out.println("The string is:"+str);
+  
+  
+	}
+}
